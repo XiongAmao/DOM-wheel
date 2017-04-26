4922abc37de4fa5324c43f5f468c4db3555c1fc3
XiongAmao <onycat@live.cn>
2017-04-26 23:55:42 +0800

    tabs component

---
+++ tabs/demo-1/README.md
@@ -0,0 +1,10 @@
+# tabs 组件完成过程：
+
+设计图：https://dribbble.com/shots/394345-Little-Tabs/attachments/21609
+
+组件设计基本原则：
+
+1. HTML CSS JS 分离
+2. 命名统一
+3. 组件可复用
+4. 约定标准
---
+++ tabs/tabs-1.js
@@ -0,0 +1,10 @@
+let selector = 'ol[data-role="tabs-nav"]>li'
+
+$('.tabs').on('click',selector,function(e){
+    let $li = $(e.currentTarget) 
+    // 使用currentTarget 保正指向this，即li标签，使后面可以复用变量$li，对li进行操作 
+    let index = $li.index()
+    $li.addClass('active').siblings().removeClass('active')
+    $li.closest('ol[data-role="tabs-nav"]').siblings('ol[data-role="tabs-panes"]')
+        .find('li').eq(index).addClass('active').siblings().removeClass('active')
+})
